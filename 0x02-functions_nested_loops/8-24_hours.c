1bc9fe1956a5ab5c8e0470ca437fcd7e93609972
Birame Ndoye <106784249+biramendoye@users.noreply.github.com>
2022-09-14 18:41:29 +0000

    Fix Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
  *
  */
 void jack_bauer(void)
 {
 	int hour, minute;
-	int max_hour = '24';
-	int max_min = '60';
+	int max_hour = 24;
+	int max_min = 60;
 
-	hour = '0';
+	hour = 0;
 	while (hour < max_hour)
 	{
-		if (hour < '10')
+		if (hour < 10)
 		{
 			_putchar('0');
 		}
-		_putchar(hour);
+		_putchar('0' + hour);
 		_putchar(':');
 
-		minute = '0';
+		minute = 0;
 		while (minute < max_min)
 		{
-			if (minute < '10')
+			if (minute < 10)
 			{
 				_putchar('0');
 			}
-			_putchar(minute);
+			_putchar('0' + minute);
 			minute++;
 			_putchar('\n');
 		}
