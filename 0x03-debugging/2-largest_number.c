7e23a13354ea4d6d8386d2251d863c0fbcb38155
Birame Ndoye <106784249+biramendoye@users.noreply.github.com>
2023-02-24 01:32:25 +0000

    C - Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,15 +10,15 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b)
-	    largest = a;
-    else
-	    largest  = b;
+	if (a > b)
+		largest = a;
+	else
+		largest  = b;
 
-    if (c > largest)
-	    largest = c;
+	if (c > largest)
+		largest = c;
 
-    return (largest);
+	return (largest);
 }
