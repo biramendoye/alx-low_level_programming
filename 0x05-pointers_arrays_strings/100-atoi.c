d0aecdb1dc43eed1f04fb2cbe4cfa4d5bb0be856
Birame Ndoye <106784249+biramendoye@users.noreply.github.com>
2023-02-28 12:53:28 +0000

    Add _atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ *
+ * @s: a string
+ *
+ * Return: an int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int output = 0;
+	int minus_sign  = 0;
+	int first_digit  = 0;
+
+	while (s[i] != '\0')
+	{
+		if (!first_digit)
+		{
+			if (s[i] == '-')
+				minus_sign++;
+		}
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			first_digit  =  1;
+			output = (output * 10) + ((int)s[i] - 48);
+		}
+
+		if (first_digit && (s[i] < 48 || s[i] > 57))
+			break;
+		i++;
+	}
+
+	if (minus_sign % 2 != 0)
+		output *= -1;
+
+	return (output);
+}
